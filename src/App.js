2abcbf91c13f9d47893d02119be998f922fe9907
sabikunnaharsafa11 <sabiunnaharsafa92@gmail.com>
2021-10-17 00:41:29 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
+import { getAuth,  signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"; 
+import { useState } from "react";
 import './App.css';
+import initializeAuthentication from './Firebase/firebasse.initialize';
+
+initializeAuthentication();
+
+const googleProvider = new GoogleAuthProvider();
+
 
 function App() {
+     const [email, setEmail] = useState('')
+     const [password, setPassword] = useState('')
+     const [error, setError] = useState('');
+     const [isLogin, setIsLogin] = useState(false);
+     const auth = getAuth();
+  const handleGoogleSignIn = () =>{
+      signInWithPopup(auth, googleProvider)
+     .then(result => {
+       const user = result.user;
+       console.log(user)
+      
+       
+     })
+  }
+  
+  const toggleLogin = e =>{
+    setIsLogin(e.target.checked);
+  }
+  const handleEmailChange = e =>{
+    setEmail(e.target.value);
+  }
+
+  const handlePasswordChange = e =>{
+    setPassword(e.target.value);
+  }
+
+  const handleRegistration = e =>{
+    e.preventDefault();
+    console.log(email, password);
+    if(password.length < 6) {
+      setError('password Must be at least 6 characters long');
+      return;
+    }
+    
+    isLogin? processLogin(email, password): registerNewUser(email, password);
+   
+  }
+
+  const processLogin = (email, password) =>{
+    signInWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user);
+    })
+    .catch(error => {
+      setError(error.message);
+    })
+  }
+
+  
+
+  const registerNewUser =(email, password) =>{
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(result => {
+      const user = result.user;
+      console.log(user)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-5">
+    <form onSubmit={handleRegistration}>
+      <h3 className="text-primary">Please {isLogin ? 'Login': 'Registor'}</h3>
+  <div className="row mb-3">
+    <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+    <div className="col-sm-10">
+      <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" required />
+    </div>
+  </div>
+  <div className="row mb-3">
+    <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+    <div className="col-sm-10">
+      <input onBlur={handlePasswordChange} type="password" className="form-control" id="inputPassword3" required />
+    </div>
+  </div>
+  
+  <div className="row mb-3">
+    <div className="col-sm-10 offset-sm-2">
+      <div className="form-check">
+        <input onChange={toggleLogin} className="form-check-input" type="checkbox" id="gridCheck1"/>
+        <label className="form-check-label" htmlFor="gridCheck1">
+         Already Registered?
+        </label>
+      </div>
+    </div>
+  </div>
+  <div className="row mb-3 text-danger">{error}</div>
+  <button type="submit" className="btn btn-primary">{isLogin ? 'Login' : 'Register'}</button>
+</form>
+    <br /><br /><br />
+    <div>---------------------------------------</div>
+    <br /><br /><br />
+      <button onClick={handleGoogleSignIn}>Google Sign in</button>
+     
+      <br/>
+      
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWQjbnRq4SwTWRvY55sxiS1MZhDwT32II",
+    authDomain: "simple-firebase-authenti-5ef77.firebaseapp.com",
+    projectId: "simple-firebase-authenti-5ef77",
+    storageBucket: "simple-firebase-authenti-5ef77.appspot.com",
+    messagingSenderId: "995074986279",
+    appId: "1:995074986279:web:87f4dbf93ad5f7840d5902"
+  };
+
+  export default firebaseConfig;
+  
